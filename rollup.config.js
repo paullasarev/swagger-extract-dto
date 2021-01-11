90566e2e43cfcf55b139fa288d85763e3b4237fa
Pavel Lasarev <Pavel_Lasarev@epam.com>
2021-01-11 12:38:51 +0400

    fix cli deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
-const { terser } = require('rollup-plugin-terser');
+// const { terser } = require('rollup-plugin-terser');
+// import resolve from 'rollup-plugin-node-resolve';
+// import commonjs from 'rollup-plugin-commonjs';
+// import json from 'rollup-plugin-json';
 // const copy = require('rollup-plugin-copy');
 
 // const isProduction = process.env.BUILD === 'production';
@@ -9,13 +12,33 @@ export default [
     output: {
       file: 'dist/swagger-extract-dto.js',
       format: 'cjs',
+      // strict: false,
+      banner: '#! /usr/bin/env node\n',
     },
     plugins: [
+      // resolve(),
+      // json(),
+      // commonjs({ include: 'node_modules/**' }),
       // terser(),
       // copy({
       //   targets: [{ src: 'src/index.d.ts', dest: 'dist', rename: 'json-schema-toolbox.d.ts' }]
       // }),
     ],
-    external: ['lodash', 'lodash/fp', 'fs', 'path', 'resolve', 'glob'],
+    external: [
+      'lodash',
+      'lodash/fp',
+      'fs',
+      'path',
+      'resolve',
+      'glob',
+      'util',
+      'yargs/yargs',
+      'yargs/helpers',
+      'mkdirp',
+      'typescript',
+      'rimraf',
+      'prettier',
+      'json-schema-ref-parser',
+    ],
   },
 ];
